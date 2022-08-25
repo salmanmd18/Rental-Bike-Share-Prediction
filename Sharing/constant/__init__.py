38e01556cd51d6fe9298504eeaa34a0a84b59aef
Mohammed Salman <salmanmohammed2018@gmail.com>
2022-08-25 14:58:56 +0530

    Model_Pusher Completed

--- Sharing/constant/__init__.py
+++ Sharing/constant/__init__.py
@@ -79,9 +79,6 @@ BEST_MODEL_KEY = "best_model"
 HISTORY_KEY = "history"
 MODEL_PATH_KEY = "model_path"
 
-EXPERIMENT_DIR_NAME="experiment"
-EXPERIMENT_FILE_NAME="experiment.csv"
-
 # Model Pusher Related variables 
 MODEL_PUSHER_CONFIG_KEY = "model_pusher_config"
 MODEL_PUSHER_MODEL_EXPORT_DIR_KEY = "model_export_dir"
--- Sharing/pipeline/pipeline.py
+++ Sharing/pipeline/pipeline.py
@@ -13,7 +13,7 @@ from sharing.component.data_validation import DataValidation
 from sharing.component.data_transformation import DataTransformation
 from sharing.component.model_trainer import ModelTrainer
 from sharing.component.model_evaluation import ModelEvaluation
-# from sharing.component.model_pusher import ModelPusher
+from sharing.component.model_pusher import ModelPusher
 
 
 class Pipeline:
@@ -71,8 +71,15 @@ class Pipeline:
         except Exception as e:
             raise SharingException(e, sys) from e
 
-    def start_model_pusher(self):
-        pass
+    def start_model_pusher(self, model_eval_artifact: ModelEvaluationArtifact) -> ModelPusherArtifact:
+        try:
+            model_pusher = ModelPusher(
+                model_pusher_config=self.config.get_model_pusher_config(),
+                model_evaluation_artifact=model_eval_artifact
+            )
+            return model_pusher.initiate_model_pusher()
+        except Exception as e:
+            raise SharingException(e, sys) from e
 
 
     def run_pipeline(self):
@@ -85,6 +92,12 @@ class Pipeline:
             model_evaluation_artifact = self.start_model_evaluation(data_ingestion_artifact=data_ingestion_artifact,
                                                                     data_validation_artifact=data_validation_artifact,
                                                                     model_trainer_artifact=model_trainer_artifact)
+            if model_evaluation_artifact.is_model_accepted:
+                model_pusher_artifact = self.start_model_pusher(model_eval_artifact=model_evaluation_artifact)
+                logging.info(f'Model pusher artifact: {model_pusher_artifact}')
+            else:
+                logging.info("Trained model rejected.")
+            logging.info("Pipeline completed.")
         except Exception as e:
             raise SharingException(e,sys) from e
 
